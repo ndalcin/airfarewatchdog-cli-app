dcb159ff070cf30281ed64f6079c312585ebb9c9
ndalcin <dalcin.nicole@gmail.com>
2017-09-28 10:06:36 -0400

    flight deal methods

--- lib/flightdeals-cli/deal.rb
+++ lib/flightdeals-cli/deal.rb
@@ -1,5 +1,39 @@
 class FlightDealCLI::Deal
-  attr_accessor :airline, :price, :departure, :arrival, :travelending, :url
+
+  attr_accessor :price, :departure, :arrival, :link, :airline, :availabledays, :startingon, :endingon, :url
+
+  @@all = []
+
+  def initialize(price, departure, arrival, link)
+    @price= price
+    @departure = departure
+    @arrival= arrival
+    @link= link
+    @@all << self
+  end
+
+  def airline
+
+  end
+
+  def availabledays
+
+  end
+
+  def startingon
+
+  end
+
+  def endingon
+
+  end
+
+  def url
+
+  end
+
+
+
 
 
 end
